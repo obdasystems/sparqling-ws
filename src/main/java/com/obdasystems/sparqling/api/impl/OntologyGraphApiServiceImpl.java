1d469a57cf2b3552606e2ceea6b6b054ab5b5194
Giacomo Ronconi <ronconi@diag.uniroma1.it>
2021-11-29 15:59:19 +0100

    Added jena and owlapi

--- src/main/java/com/obdasystems/sparqling/api/impl/OntologyGraphApiServiceImpl.java
+++ src/main/java/com/obdasystems/sparqling/api/impl/OntologyGraphApiServiceImpl.java
@@ -13,6 +13,8 @@ import com.obdasystems.sparqling.api.NotFoundException;
 import java.io.InputStream;
 
 import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
 
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.SecurityContext;
@@ -20,7 +22,7 @@ import javax.validation.constraints.*;
 @javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.JavaJerseyServerCodegen", date = "2021-11-29T11:28:53.694Z[GMT]")public class OntologyGraphApiServiceImpl extends OntologyGraphApiService {
     @Override
     public Response highligths( @NotNull String clickedClassIRI,  List<String> params, SecurityContext securityContext) throws NotFoundException {
-        // do some magic!
+        OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
         return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "magic!")).build();
     }
     @Override
